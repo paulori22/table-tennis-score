9dc8b2e3096e2cf7c424865ea1e84897aa6b3c8a
paulori22 <paulinhoboneti@yahoo.com.br>
2024-05-10 09:21:37 -0300

    Fixing url issue for api production - Created new env var for production NEXT_PUBLIC_URL - Check if variavel is set then baseUrl will use it

--- src/util/http/index.ts
+++ src/util/http/index.ts
@@ -1,11 +1,13 @@
 import axios from "axios";
 
 export const getBaseApiUrl = () => {
+  const baseUrlEnv =
+    process.env.NEXT_PUBLIC_URL ?? process.env.NEXT_PUBLIC_VERCEL_URL;
   if (process.env.NEXT_PUBLIC_VERCEL_ENV === "development") {
-    return `http://${process.env.NEXT_PUBLIC_VERCEL_URL}/api`;
+    return `http://${baseUrlEnv}/api`;
   }
 
-  return `https://${process.env.NEXT_PUBLIC_VERCEL_URL}/api`;
+  return `https://${baseUrlEnv}/api`;
 };
 
 export const scoreApi = axios.create({
